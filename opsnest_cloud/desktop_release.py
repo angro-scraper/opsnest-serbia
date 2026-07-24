0ae6c7fb78352e5eaf10dd737cfba54fe19e97d3
angro-scraper <trajkoff82@gmail.com>
2026-07-24 20:28:08 +0200

    Publish desktop update 2.10.0

--- opsnest_cloud/desktop_release.py
+++ opsnest_cloud/desktop_release.py
@@ -12,9 +12,9 @@ import re
 
 
 FALLBACK_RELEASE = {
-    "latest_version": "2.9.9",
-    "installer_url": "https://opsnestone.com/downloads/OpsNest-Setup-2.9.9.exe",
-    "installer_sha256": "c9ffdf11496f7205a67021a1f9261074c67b0b8d6b27d61c6822495b55ed916b",
+    "latest_version": "2.10.0",
+    "installer_url": "https://opsnestone.com/downloads/OpsNest-Setup-2.10.0.exe",
+    "installer_sha256": "ab8fe711054fd849e731ab462ab81752c449e6d42d526a7ff618599af10ae618",
 }
 
 
