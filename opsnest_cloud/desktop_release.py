c6dac681935bf886e99d34faa4ce68daab0ae428
angro-scraper <trajkoff82@gmail.com>
2026-07-29 12:05:20 +0200

    release: publish desktop 2.13.9

--- opsnest_cloud/desktop_release.py
+++ opsnest_cloud/desktop_release.py
@@ -13,9 +13,9 @@ from urllib.parse import urlparse
 
 
 FALLBACK_RELEASE = {
-    "latest_version": "2.13.7",
-    "installer_url": "https://opsnestone.com/downloads/OpsNest-Setup-2.13.7.exe",
-    "installer_sha256": "cfb39ba2d25052179fa0bd3e65b74a0ce9fc031ae36cfc239aa67a12268a8adb",
+    "latest_version": "2.13.9",
+    "installer_url": "https://opsnestone.com/downloads/OpsNest-Setup-2.13.9.exe",
+    "installer_sha256": "b16737ccbb64f84935669cb472d4b99de853ffa64833e361da9f10e95761a3e3",
 }
 _RELEASE_VERSION = re.compile(r"^\d+\.\d+\.\d+$")
 _TRUSTED_DOWNLOAD_HOSTS = {"opsnestone.com", "www.opsnestone.com"}
